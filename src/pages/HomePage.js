9ed5bb143c946982ea3ac700f9121a3d9f54e64d
stark <397866153@qq.com>
2021-08-24 11:02:40 +0800

    no message

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, Button, TextInput, TouchableOpacity } from 'react-native'
 import { useTheme } from '@react-navigation/native'
 import { SafeView } from '@components'
 import { useSelector, useDispatch } from 'react-redux'
-import { tool, appStyle, XView, XWidget, XText, XSize, List, ahooks, MyStyleSheet } from 'react-native-common-tools'
+import { ScrollView, appStyle, XView, XWidget, XText, XSize, List, ahooks, MyStyleSheet } from 'react-native-common-tools'
 import { captureMessage, sentryLog } from '../sentry/sentry'
 import { FormattedMessage } from 'react-intl'
 import { useDrawerNavigator, useNavFocusListener, useBannerModel, useIntlModel } from '@useHooks'
@@ -85,26 +85,7 @@ const HomePage = ({ navigation, route }) => {
 
   return (
     <SafeView>
-       {/* https://bolan9999.github.io/react-native-spring-scrollview/#/zh-cn/V3/BasicContent 必须有一个确定的高度才能正常工作，因为它实际上所做的就是将一系列不确定高度的子组件装进一个确定高度的容器（通过滚动操作）。SpringScrollView默认具有{flex:1}的样式，因此要使SpringScrollView正常工作，它的父容器必须是确定高度的，你也可以通过手动指定样式，使之正常工作。 */}
-      <SpringScrollView
-        // style控制外层包裹视图的样式
-        style={{ width: '100%' }}
-        // contentStyle控制内层视图的样式
-        contentStyle={{ width: '100%', backgroundColor: 'lightgray' }}
-        bounces={true} scrollEnabled={true} showsVerticalScrollIndicator={true}
-        // 支持双向滑动的情况下(也就是 子节点的内容超过了 content 的范围)，控制一次滑动是否只允许水平或垂直一个方向
-        directionalLockEnabled={true}
-        // 拖拽 SpringScrollView 是否收起键盘
-        dragToHideKeyboard={true}
-        // 不同的系统，不同的三方输入法，键盘的工具栏高度是不确定的，并且官方没有给出获取工具栏高度的办法，这个属性用以给用户小幅调整键盘弹起时，组件偏移的位置
-        inputToolBarHeight={44}
-        // 当值为 true 时，滚动条会停在设置的pageSize整数倍位置。这个属性在iOS和安卓上都支持双向分页。
-        pagingEnabled={false}
-        // 配合pagingEnabled使用分页，使滑动停止在设置的整数倍位置。同时支持水平和垂直双向分页。0代表使用SpringScrollView的视口大小。
-        pageSize={{ width: 0, height: 0 }}
-        // 将TextInput的引用传入，让SpringScrollView自动管理键盘遮挡问题
-        textInputRefs={[_topInput]}
-        initialContentOffset={{ x: 0, y: 0 }}>
+      <ScrollView textInputRefs={[_topInput]}>
         <View style={[styles.row, { }]}>
           <Button
             title='Go to details screen'
@@ -164,7 +145,8 @@ const HomePage = ({ navigation, route }) => {
           returnKeyType="next"
           placeholder="Keyboard Test Top"
         />
-      </SpringScrollView>
+      </ScrollView>
+
     </SafeView>
   )
 }
